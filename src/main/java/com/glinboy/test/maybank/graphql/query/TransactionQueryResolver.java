768c2af6655104eae339335a87afebd9e03058e5
GLinBoy <hojjatabedie@gmail.com>
2020-08-01 17:59:08 +0800

    add pagination & search by customer ID or account number(s) or description

--- src/main/java/com/glinboy/test/maybank/graphql/query/TransactionQueryResolver.java
+++ src/main/java/com/glinboy/test/maybank/graphql/query/TransactionQueryResolver.java
@@ -2,6 +2,7 @@ package com.glinboy.test.maybank.graphql.query;
 
 import java.util.List;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.glinboy.test.maybank.model.Transaction;
@@ -18,7 +19,24 @@ public class TransactionQueryResolver implements GraphQLQueryResolver {
 		this.transactionService = transactionService;
 	}
 	
-	public List<Transaction> getTransactions() {
-		return this.transactionService.getAll();
+	public List<Transaction> getTransactions(Integer page, Integer size) {
+		PageRequest pageable = PageRequest.of(page != null ? page : 0, size != null ? size : 0);
+		return this.transactionService.getAll(pageable).getContent();
 	}
+	
+	public List<Transaction> getTransactionsByCustomerId(Long customerId, Integer page, Integer size) {
+		PageRequest pageable = PageRequest.of(page != null ? page : 0, size != null ? size : 0);
+		return this.transactionService.getTransactionsByCustomerId(customerId, pageable).getContent();
+	}
+	
+	public List<Transaction> getTransactionsByAccountNumber(String accountNumber, Integer page, Integer size) {
+		PageRequest pageable = PageRequest.of(page != null ? page : 0, size != null ? size : 0);
+		return this.transactionService.getTransactionsByAccountNumber(accountNumber, pageable).getContent();
+	}
+	
+	public List<Transaction> getTransactionsByDescription(String description, Integer page, Integer size) {
+		PageRequest pageable = PageRequest.of(page != null ? page : 0, size != null ? size : 0);
+		return this.transactionService.getTransactionsByDescription(description, pageable).getContent();
+	}
+	
 }
--- src/main/java/com/glinboy/test/maybank/repository/TransactionRepository.java
+++ src/main/java/com/glinboy/test/maybank/repository/TransactionRepository.java
@@ -1,5 +1,7 @@
 package com.glinboy.test.maybank.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -7,5 +9,11 @@ import com.glinboy.test.maybank.model.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long>,
 	JpaSpecificationExecutor<Transaction> {
+
+	Page<Transaction> findAllByCustomerId(Long customerId, Pageable pageable);
+
+	Page<Transaction> findAllByAccountNumber(String accountNumber, Pageable pageable);
+
+	Page<Transaction> findAllByDescription(String description, Pageable pageable);
 
 }
