3f9be1ce7f20619e42b180143f95b34f3ef3cadf
GLinBoy <hojjatabedie@gmail.com>
2020-08-02 12:19:15 +0800

    fix auditing

--- src/main/java/com/glinboy/test/maybank/config/AuditingConfig.java
+++ src/main/java/com/glinboy/test/maybank/config/AuditingConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.glinboy.test.maybank.security.UserPrincipal;
 
 @Configuration
 @EnableJpaAuditing
@@ -21,6 +26,15 @@ class SpringSecurityAuditAwareImpl implements AuditorAware<String> {
 
 	@Override
 	public Optional<String> getCurrentAuditor() {
-		return Optional.ofNullable("anonymouse");
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication == null || !authentication.isAuthenticated()
+				|| authentication instanceof AnonymousAuthenticationToken) {
+			return Optional.empty();
+		}
+
+		UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+		return Optional.ofNullable(userPrincipal.getUsername());
 	}
 }
